33c042bc4f01c11f0e425d7eb5ba3775fb99ad48
Ali Njie <apfnjie@gmail.com>
2015-10-30 15:32:45 +0000

    New
    functions added
    get latest new (x)
    get specific news item
    get all new items
    update news item

--- model/news.php
+++ model/news.php
@@ -11,37 +11,46 @@ class News extends adb{
   /**
    * Function to add new news item
    */
-    function add_news($picture_path, $info){
-    	$str_sql = "INSERT INTO team_news (picture_path, news_info) VALUES ('$picture', '$path')";
+    function add_news($headline, $picture_path, $info) {
+    	$str_sql = "INSERT INTO team_news (headline, picture_path, news_info) VALUES ('$headline', '$picture', '$path')";
     	return $this->query($str_sql);
     }
 
     /**
-     * function to delete a news item
+     * function to find a news item
      */
-    function delete_news($id){
-    	$str_sql = "DELETE from team_news WHERE id=$id";
-        return $this->query($str_sql);
-    }
-
-    function find_news($id){
-    	$str_sql = " SELECT * from news WHERE id=$id LIMIT 0,1";
+    function get_news_item($id) {
+    	$str_sql = "SELECT * from team_news WHERE id=$id LIMIT 0";
     	if(!$this->query($str_sql)){
             return false;
         }
     	return $this->fetch();
     }
 
-    function all_news(){
-    	$str_sql = "SELECT * from news ORDER BY date_added DESC";
+    /**
+     * function to get all news item
+     */
+    function get_all_news() {
+    	$str_sql = "SELECT * from team_news ORDER BY date_time DESC";
     	if(!$this->query($str_sql)){
             return false;
         }
     	return $this->fetch();
     }
 
-    function latest_news(){
-        $str_sql = "SELECT * from news order by news_date desc limit 2";
+    /**
+     * function to update news item
+     */
+     function update_news_item($id, $headline, $path, $info) {
+       $str_sql = "UPDATE team_news SET headline = '$headline', picture_path = '$path', news_info = '$info' WHERE id = '$id'";
+       return $this->query($str_sql);
+     }
+
+    /**
+     * function to get x latest news iitem
+     */
+    function get_latest_news($number) {
+        $str_sql = "SELECT * from team_news order by date_time desc limit $number";
         if(!$this->query($str_sql)){
             return false;
         }
